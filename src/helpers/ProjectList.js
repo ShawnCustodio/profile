cbecf4da3730910983d2d836f04d16a561a291ee
Shawn Custodio <shawncustodiomail@gmail.com>
2023-09-14 00:57:10 -0500

    cool bars

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -9,29 +9,34 @@ export const ProjectList = [
         name: "Basic Piano",
         image: PianoG,
         skill: "JavaScript, HTML, CSS",
-        link: "https://github.com/ShawnCustodio/ShawnCustodio.github.io"
+        link: "https://github.com/ShawnCustodio/ShawnCustodio.github.io",
+        desc: "This is a React Project that revolves around being able to use MP4 onClick"
     },
     {
         name: "Todo-List",
         image: TL,
         skill: "JavaScript, HTML, CSS",
-        link: "https://github.com/ShawnCustodio/todo-list"
+        link: "https://github.com/ShawnCustodio/todo-list",
+        desc: "This is a React Project that revolves around being able to use hashmaps and inputs"
     },
     {
         name: "JavaFX + SQL",
         image: JFX,
-        skill: "Java, JavaFX, SQL, Sqlite"
+        skill: "Java, JavaFX, SQL, Sqlite",
+        desc: "This is a Java and JDBC Project that revolves around being able to use SQLite3 in order to manipulate a Database"
     },
     {
         name: "Calculator",
         image: Calc,
         skill: "React, JavaScript, HTML, CSS",
-        link: "https://github.com/ShawnCustodio/calculator"
+        link: "https://github.com/ShawnCustodio/calculator",
+        desc: "This is a React Project that revolves around understanding parsing through files and being able to communicate"
     },
     {
         name: "Zara Kaggle Dataset",
         image: ZK,
         skill: "Python, Pandas, Sklearn, Numpy, Matplotlib ",
-        link: "https://colab.research.google.com/drive/1cka6ZQXoiuZWSUG5aD6iV-Q4MqfSK1LF?usp=sharing"
+        link: "https://colab.research.google.com/drive/1cka6ZQXoiuZWSUG5aD6iV-Q4MqfSK1LF?usp=sharing",
+        desc: "This is a Python Project that revolves around using Pandas, Sklearn, and Numpy in order to learn Data Computational Methods"
     }
 ]
--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import '../styles/ProjectDisplay.css';
+import WorkIcon from "@material-ui/icons/Work";
+
 
 function ProjectDisplay() {
   const { id } = useParams();
@@ -16,11 +18,34 @@ function ProjectDisplay() {
     <div className="project">
       <h1> {project.name}</h1>
       <img src={project.image} />
-      <p>
-        <b>Skills:</b> {project.skill}
-      </p>
-      <a href={project.link} target="_blank" rel="noopener noreferrer" style={iconStyle}><GitHubIcon /></a>
+   
+
+      <ul className = "move">
+    <li className = "banner0">
+        <div class="icon"><i class="fa-brands fa-codepen"></i></div>
+        <div class="title">Description</div>
+        <div class="descr">{project.desc}</div>
+    </li>
+    <li className= "banner3" >
+        <div class="icon"><i class="fa-brands fa-js"></i></div>
+        <div class="title">Skills</div>
+        <div class="descr"> {project.skill}</div>
+    </li>
+    <li className = "banner4">
+    <a href={project.link} target="_blank" rel="noopener noreferrer" style={iconStyle}><GitHubIcon /></a>
+        <div class="title">Github</div>
+        <div class="descr">Github Link!</div>
+    </li>
+</ul>
+
+
+
+
+
     </div>
+
+
+
   );
 }
 
